ac063d8a205acd2ff63979fdcea1cd77e1ef72a9
Justin A <jarata@gmail.com>
2020-03-10 15:18:55 -0700

    Added noted for Step 1

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,4 @@
+# Step 1.2
 from django.urls import path
 
 from . import views
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Step 1.1
 from django.http import HttpResponse
 
 
